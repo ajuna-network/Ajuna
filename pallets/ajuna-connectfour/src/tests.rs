801472c4b4765f9fba9ad5bdf628e9541ab92688
Andy Bell <andyjsbell@gmail.com>
2022-05-16 13:58:46 +0200

    clean up connect four

--- pallets/ajuna-connectfour/src/tests.rs
+++ pallets/ajuna-connectfour/src/tests.rs
@@ -136,35 +136,6 @@ fn test_game_play() {
 }
 
 #[test]
-fn test_game_events() {
-	new_test_ext().execute_with(|| {
-		let blocks_to_pass = 10;
-		let mut current_block: u64 = 100;
-
-		// start from block 100
-		run_to_block(current_block);
-
-		assert_eq!(None, ConnectFour::something());
-
-		// Test game creation between to different players
-		assert_ok!(ConnectFour::new_game(
-			Origin::signed(PLAYER_1 as u64),
-			PLAYER_1 as u64,
-			PLAYER_2 as u64
-		));
-
-		run_next_block();
-		current_block += 1;
-
-		assert_eq!(None, ConnectFour::something());
-
-		run_to_block(current_block + blocks_to_pass);
-
-		assert_eq!(77, ConnectFour::something().unwrap());
-	});
-}
-
-#[test]
 fn test_force_turn() {
 	new_test_ext().execute_with(|| {
 		let mut current_block: u64 = 100;
@@ -225,67 +196,5 @@ fn test_force_turn() {
 		assert!(!PlayerBoard::<Test>::contains_key(board.red));
 		assert!(!PlayerBoard::<Test>::contains_key(board.blue));
 		assert!(!BoardSchedules::<Test>::contains_key(board_id));
-	});
-}
-
-#[test]
-fn test_matchmaker_game() {
-	new_test_ext().execute_with(|| {
-		let mut current_block: u64 = 100;
-
-		// start from block 100
-		run_to_block(current_block);
-
-		// queue up player 1
-		assert_ok!(ConnectFour::queue(Origin::signed(PLAYER_1 as u64)));
-
-		run_to_block(current_block + 1);
-		current_block += 1;
-
-		// try to queue again same player 1
-		assert_noop!(
-			ConnectFour::queue(Origin::signed(PLAYER_1 as u64)),
-			Error::<Test>::AlreadyQueued
-		);
-
-		// queue up player 2
-		assert_ok!(ConnectFour::queue(Origin::signed(PLAYER_2 as u64)));
-
-		assert!(!PlayerBoard::<Test>::contains_key(PLAYER_1 as u64));
-
-		run_to_block(current_block + 1);
-		current_block += 1;
-
-		assert!(PlayerBoard::<Test>::contains_key(PLAYER_1 as u64));
-
-		let board_id = PlayerBoard::<Test>::get(PLAYER_1 as u64);
-		let board = ConnectFour::boards(board_id).expect("board should exist");
-
-		assert_eq!(board.blue, PLAYER_2 as u64);
-
-		if board.next_player == PLAYER_1 {
-			assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_1 as u64), 0));
-			let board = ConnectFour::boards(board_id).expect("board should exist");
-			assert!(board.board_state == BoardState::Running);
-			assert!(board.next_player == PLAYER_2);
-			assert_eq!(board.last_turn, current_block);
-
-			run_next_block();
-			current_block += 1;
-		}
-
-		assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_2 as u64), 1));
-		let board = ConnectFour::boards(board_id).expect("board should exist");
-		assert_eq!(board.last_turn, current_block);
-		assert!(board.board_state == BoardState::Running);
-		assert!(board.next_player == PLAYER_1);
-
-		run_to_block(current_block + 10);
-		current_block += 10;
-
-		// check if force turn ended the game
-		let board = ConnectFour::boards(board_id).expect("board should exist");
-		assert_eq!(board.last_turn, current_block);
-		assert!(board.board_state == BoardState::Finished(board.blue));
 	});
 }
