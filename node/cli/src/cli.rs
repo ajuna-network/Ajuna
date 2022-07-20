4d24e5fc6e1b9f58ebb4903a676ad109a89337f7
Eric <cowboy-bebug@users.noreply.github.com>
2022-07-20 12:43:39 +1200

    add missing ajuna feature for cli

--- node/cli/src/cli.rs
+++ node/cli/src/cli.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#[cfg(feature = "bajun")]
+#[cfg(any(feature = "bajun", feature = "ajuna"))]
 use {
 	clap::Parser,
 	sc_cli::{
