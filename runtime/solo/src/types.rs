6abd9e0d099dd50398cd3fcad8157f2109907f42
Didac Semente Fernandez <didac@ajuna.io>
2022-10-03 13:32:47 +0200

    * Added pallet-proxy and pallet-identity to solo runtime

--- runtime/solo/src/types.rs
+++ runtime/solo/src/types.rs
@@ -36,3 +36,45 @@ pub mod governance {
 		EnsureProportionAtLeast<AccountId, CouncilCollective, 3, 4>;
 	pub(crate) type EnsureAllCouncil = EnsureProportionAtLeast<AccountId, CouncilCollective, 1, 1>;
 }
+
+pub mod proxy {
+	use codec::{Decode, Encode};
+	use frame_support::{pallet_prelude::MaxEncodedLen, traits::InstanceFilter, RuntimeDebug};
+	use scale_info::TypeInfo;
+
+	/// Proxy type enum lists the type of calls that are supported by the proxy
+	/// pallet
+	#[derive(
+		Copy,
+		Clone,
+		Eq,
+		PartialEq,
+		Ord,
+		PartialOrd,
+		MaxEncodedLen,
+		Decode,
+		Encode,
+		RuntimeDebug,
+		TypeInfo,
+	)]
+	pub enum ProxyType {
+		Any,
+	}
+
+	impl Default for ProxyType {
+		fn default() -> Self {
+			Self::Any
+		}
+	}
+
+	impl<Call> InstanceFilter<Call> for ProxyType {
+		fn filter(&self, _c: &Call) -> bool {
+			match self {
+				ProxyType::Any => true,
+			}
+		}
+		fn is_superset(&self, o: &Self) -> bool {
+			self == &ProxyType::Any || self == o
+		}
+	}
+}
