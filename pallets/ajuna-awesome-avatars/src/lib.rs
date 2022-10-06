5859fa3df6b845857b15a5c342d1c903a4789176
Eric <cowboy-bebug@users.noreply.github.com>
2022-10-07 09:52:38 +1300

    make extrinsics available by default

--- pallets/ajuna-awesome-avatars/src/lib.rs
+++ pallets/ajuna-awesome-avatars/src/lib.rs
@@ -92,7 +92,7 @@ pub mod pallet {
 		GlobalConfig {
 			max_avatars_per_player: 1_000,
 			mint: MintConfig {
-				open: false,
+				open: true,
 				fees: MintFees {
 					one: (1_000_000_000_000_u64 * 55 / 100).unique_saturated_into(),
 					three: (1_000_000_000_000_u64 * 50 / 100).unique_saturated_into(),
@@ -101,8 +101,8 @@ pub mod pallet {
 				cooldown: 5_u8.into(),
 				free_mint_transfer_fee: 1,
 			},
-			forge: ForgeConfig { open: false, min_sacrifices: 1, max_sacrifices: 4 },
-			trade: TradeConfig { open: false },
+			forge: ForgeConfig { open: true, min_sacrifices: 1, max_sacrifices: 4 },
+			trade: TradeConfig { open: true },
 		}
 	}
 	#[pallet::storage]
