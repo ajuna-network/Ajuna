d7dcbb80224cd519c776dda958972c7e2aed1eea
Eric <6591791+cowboy-bebug@users.noreply.github.com>
2022-10-27 08:47:24 +1300

    [AAATAR-191] Benchmarks (#129)
    * enable runtime-benchmarks
    
    * add benchmarks for issue_free_mints
    
    * add benchmarks for update_global_config
    
    * add benchmarks for set_season
    
    * add benchmarks for set_treasurer
    
    * add benchmarks for set_organizer
    
    * add benchmarks for mint
    
    * add benchmarks for forge
    
    * add benchmarks for transfer_free_mints
    
    * add benchmarks for set_price
    
    * add benchmarks for remove_price
    
    * add benchmarks for buy
    
    * fix origin
    
    * fix overflow

--- pallets/ajuna-awesome-avatars/src/benchmarking.rs
+++ pallets/ajuna-awesome-avatars/src/benchmarking.rs
@@ -17,10 +17,267 @@
 use super::*;
 
 #[allow(unused)]
-use crate::Pallet as Template;
-use frame_benchmarking::{benchmarks, whitelisted_caller};
+use crate::{
+	types::{RarityTier::*, *},
+	Pallet as AAvatars,
+};
+use frame_benchmarking::{benchmarks, vec};
+use frame_support::traits::Currency;
 use frame_system::RawOrigin;
+use sp_runtime::traits::UniqueSaturatedFrom;
+
+fn account<T: Config>(name: &'static str) -> T::AccountId {
+	let index = 0;
+	let seed = 0;
+	frame_benchmarking::account(name, index, seed)
+}
+
+fn create_seasons<T: Config>(n: usize) -> Result<(), &'static str> {
+	CurrentSeasonId::<T>::put(1);
+	CurrentSeasonStatus::<T>::put(SeasonStatus {
+		active: true,
+		early: false,
+		prematurely_ended: false,
+	});
+	for i in 0..n {
+		Seasons::<T>::insert(
+			(i + 1) as SeasonId,
+			Season {
+				name: [u8::MAX; 100].to_vec().try_into().unwrap(),
+				description: [u8::MAX; 1_000].to_vec().try_into().unwrap(),
+				early_start: T::BlockNumber::from((i * 10 + 1) as u32),
+				start: T::BlockNumber::from((i * 10 + 5) as u32),
+				end: T::BlockNumber::from((i * 10 + 10) as u32),
+				max_tier_forges: u32::MAX,
+				max_variations: 15,
+				max_components: 16,
+				tiers: vec![Common, Uncommon, Rare, Epic, Legendary, Mythical].try_into().unwrap(),
+				p_single_mint: vec![70, 20, 5, 4, 1].try_into().unwrap(),
+				p_batch_mint: vec![40, 30, 15, 10, 5].try_into().unwrap(),
+			},
+		);
+	}
+	frame_system::Pallet::<T>::set_block_number(
+		AAvatars::<T>::seasons(AAvatars::<T>::current_season_id()).unwrap().start,
+	);
+	Ok(())
+}
+
+fn create_avatars<T: Config>(name: &'static str, n: usize) -> Result<(), &'static str> {
+	create_seasons::<T>(3)?;
+
+	let player = account::<T>(name);
+	let season_id = 1;
+	FreeMints::<T>::insert(&player, n as MintCount);
+	for _ in 0..n {
+		AAvatars::<T>::do_mint(
+			&player,
+			&MintOption { mint_type: MintType::Free, count: MintPackSize::One },
+			season_id,
+		)?;
+		LastMintedBlockNumbers::<T>::remove(&player);
+	}
+	Ok(())
+}
+
+fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
+	frame_system::Pallet::<T>::assert_last_event(generic_event.into());
+}
 
 benchmarks! {
-	impl_benchmark_test_suite!(Template, crate::mock::always_good_to_test(), crate::mock::Test);
+	mint_free {
+		let name = "player";
+		let n in 0 .. (AAvatars::<T>::global_configs().max_avatars_per_player - 6);
+		let n = n as usize;
+		create_avatars::<T>(name, n)?;
+
+		let caller = account::<T>(name);
+		FreeMints::<T>::insert(&caller, MintCount::MAX);
+
+		let mint_option = MintOption { mint_type: MintType::Free, count: MintPackSize::Six };
+	}: mint(RawOrigin::Signed(caller.clone()), mint_option)
+	verify {
+		let avatar_ids = AAvatars::<T>::owners(caller)[n..(n + 6)].to_vec();
+		assert_last_event::<T>(Event::AvatarsMinted { avatar_ids }.into())
+	}
+
+	mint_normal {
+		let name = "player";
+		let n in 0 .. (AAvatars::<T>::global_configs().max_avatars_per_player - 6);
+		let n = n as usize;
+		create_avatars::<T>(name, n)?;
+
+		let caller = account::<T>(name);
+		let mint_fee = AAvatars::<T>::global_configs().mint.fees.fee_for(&MintPackSize::Six);
+		T::Currency::make_free_balance_be(&caller, mint_fee);
+
+		let mint_option = MintOption { mint_type: MintType::Normal, count: MintPackSize::Six };
+	}: mint(RawOrigin::Signed(caller.clone()), mint_option)
+	verify {
+		let avatar_ids = AAvatars::<T>::owners(caller)[n..(n + 6)].to_vec();
+		assert_last_event::<T>(Event::AvatarsMinted { avatar_ids }.into())
+	}
+
+	forge {
+		let name = "player";
+		let max_avatars = AAvatars::<T>::global_configs().max_avatars_per_player as usize;
+		create_avatars::<T>(name, max_avatars)?;
+
+		let player = account::<T>(name);
+		let avatar_ids = AAvatars::<T>::owners(&player);
+		let avatar_id = avatar_ids[0];
+		let (_owner, original_avatar) = AAvatars::<T>::avatars(&avatar_id).unwrap();
+	}: _(RawOrigin::Signed(player), avatar_id, avatar_ids[1..5].to_vec())
+	verify {
+		let (_owner, upgraded_avatar) = AAvatars::<T>::avatars(&avatar_id).unwrap();
+		let original_tiers = original_avatar.dna.into_iter().map(|x| x >> 4);
+		let upgraded_tiers = upgraded_avatar.dna.into_iter().map(|x| x >> 4);
+		let upgraded_components = original_tiers.zip(upgraded_tiers).fold(
+			0, |mut count, (lhs, rhs)| {
+				if lhs != rhs {
+					count+=1;
+				}
+				count
+			}
+		);
+		assert_last_event::<T>(Event::AvatarForged { avatar_id, upgraded_components }.into())
+	}
+
+	transfer_free_mints {
+		let from = account::<T>("from");
+		let to = account::<T>("to");
+		let free_mint_transfer_fee = AAvatars::<T>::global_configs().mint.free_mint_transfer_fee;
+		let how_many = MintCount::MAX - free_mint_transfer_fee as MintCount;
+		FreeMints::<T>::insert(&from, MintCount::MAX);
+	}: _(RawOrigin::Signed(from.clone()), to.clone(), how_many)
+	verify {
+		assert_last_event::<T>(Event::FreeMintsTransferred { from, to, how_many }.into())
+	}
+
+	set_price {
+		let name = "player";
+		let max_avatars = AAvatars::<T>::global_configs().max_avatars_per_player as usize;
+		create_avatars::<T>(name, max_avatars)?;
+		let caller = account::<T>(name);
+		let avatar_id = AAvatars::<T>::owners(&caller)[0];
+		let price = BalanceOf::<T>::unique_saturated_from(u128::MAX);
+	}: _(RawOrigin::Signed(caller), avatar_id, price)
+	verify {
+		assert_last_event::<T>(Event::AvatarPriceSet { avatar_id, price }.into())
+	}
+
+	remove_price {
+		let name = "player";
+		let max_avatars = AAvatars::<T>::global_configs().max_avatars_per_player as usize;
+		create_avatars::<T>(name, max_avatars)?;
+		let caller = account::<T>(name);
+		let avatar_id = AAvatars::<T>::owners(&caller)[0];
+		Trade::<T>::insert(&avatar_id, BalanceOf::<T>::unique_saturated_from(u128::MAX));
+	}: _(RawOrigin::Signed(caller), avatar_id)
+	verify {
+		assert_last_event::<T>(Event::AvatarPriceUnset { avatar_id }.into())
+	}
+
+	buy {
+		let (buyer_name, seller_name) = ("buyer", "seller");
+		let (buyer, seller) = (account::<T>(buyer_name), account::<T>(seller_name));
+		let max_avatars = AAvatars::<T>::global_configs().max_avatars_per_player as usize;
+		create_avatars::<T>(buyer_name, max_avatars - 1)?;
+		create_avatars::<T>(seller_name, max_avatars)?;
+
+		let buy_fee = AAvatars::<T>::global_configs().trade.buy_fee;
+		let sell_fee = BalanceOf::<T>::unique_saturated_from(u64::MAX / 2);
+		T::Currency::make_free_balance_be(&buyer, sell_fee + buy_fee);
+		T::Currency::make_free_balance_be(&seller, sell_fee);
+
+		let avatar_id = AAvatars::<T>::owners(&seller)[0];
+		Trade::<T>::insert(&avatar_id, sell_fee);
+	}: _(RawOrigin::Signed(buyer.clone()), avatar_id)
+	verify {
+		assert_last_event::<T>(Event::AvatarTraded { avatar_id, from: seller, to: buyer }.into())
+	}
+
+	set_organizer {
+		let organizer = account::<T>("organizer");
+	}: _(RawOrigin::Root, organizer.clone())
+	verify {
+		assert_last_event::<T>(Event::OrganizerSet { organizer }.into())
+	}
+
+	set_treasurer {
+		let treasurer = account::<T>("treasurer");
+	}: _(RawOrigin::Root, treasurer.clone())
+	verify {
+		assert_last_event::<T>(Event::TreasurerSet { treasurer }.into())
+	}
+
+	set_season {
+		let organizer = account::<T>("organizer");
+		Organizer::<T>::put(&organizer);
+
+		let season_id = 1;
+		let season = Season {
+			name: [u8::MAX; 100].to_vec().try_into().unwrap(),
+			description: [u8::MAX; 1_000].to_vec().try_into().unwrap(),
+			early_start: T::BlockNumber::from(u32::MAX - 2),
+			start: T::BlockNumber::from(u32::MAX - 1),
+			end: T::BlockNumber::from(u32::MAX),
+			max_tier_forges: u32::MAX,
+			max_variations: 15,
+			max_components: 16,
+			tiers: vec![Common, Uncommon, Rare, Epic, Legendary, Mythical].try_into().unwrap(),
+			p_single_mint: vec![70, 20, 5, 4, 1].try_into().unwrap(),
+			p_batch_mint: vec![40, 30, 15, 10, 5].try_into().unwrap(),
+		};
+	}: _(RawOrigin::Signed(organizer), season_id, season.clone())
+	verify {
+		assert_last_event::<T>(Event::UpdatedSeason { season_id, season }.into())
+	}
+
+	update_global_config {
+		let organizer = account::<T>("organizer");
+		Organizer::<T>::put(&organizer);
+
+		let config = GlobalConfig {
+			max_avatars_per_player: u32::MAX,
+			mint: MintConfig {
+				open: true,
+				fees: MintFees {
+					one: BalanceOf::<T>::unique_saturated_from(u128::MAX),
+					three: BalanceOf::<T>::unique_saturated_from(u128::MAX),
+					six: BalanceOf::<T>::unique_saturated_from(u128::MAX),
+				},
+				cooldown: T::BlockNumber::from(u32::MAX),
+				free_mint_fee_multiplier: MintCount::MAX,
+				free_mint_transfer_fee: MintCount::MAX,
+			},
+			forge: ForgeConfig {
+				open: true,
+				min_sacrifices: u8::MAX,
+				max_sacrifices: u8::MAX,
+			},
+			trade: TradeConfig {
+				open: true,
+				buy_fee: BalanceOf::<T>::unique_saturated_from(u128::MAX),
+			}
+		};
+	}: _(RawOrigin::Signed(organizer), config.clone())
+	verify {
+		assert_last_event::<T>(Event::UpdatedGlobalConfig(config).into())
+	}
+
+	issue_free_mints {
+		let organizer = account::<T>("organizer");
+		Organizer::<T>::put(&organizer);
+
+		let to = account::<T>("to");
+		let how_many = MintCount::MAX;
+	}: _(RawOrigin::Signed(organizer), to.clone(), how_many)
+	verify {
+		assert_last_event::<T>(Event::FreeMintsIssued { to, how_many }.into())
+	}
+
+	impl_benchmark_test_suite!(
+		AAvatars, crate::mock::ExtBuilder::default().build(), crate::mock::Test
+	);
 }
