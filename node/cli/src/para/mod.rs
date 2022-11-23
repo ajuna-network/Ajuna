6cfbbb227efe53a19c9967d26a4e418130af0837
Eric <cowboy-bebug@users.noreply.github.com>
2022-11-23 14:49:11 +1300

    feat: extract into para mod

--- node/cli/src/para/mod.rs
+++ node/cli/src/para/mod.rs
@@ -14,9 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#[cfg(feature = "para")]
-pub mod para;
-#[cfg(feature = "solo")]
-pub mod solo;
+mod cli;
+mod command;
 
-pub use sc_cli::Result;
+pub use command::run;
