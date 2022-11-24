d36e0a151a59c2f55a1f20404f9b38e4b55cf1ba
Eric <cowboy-bebug@users.noreply.github.com>
2022-11-24 13:59:12 +1300

    feat: upgrade weight script

--- .maintain/weights.sh
+++ .maintain/weights.sh
@@ -37,6 +37,7 @@ for PALLET in "${PALLETS[@]}"; do
     --output="./runtime/${RUNTIME}/src/weights/${PALLET//-/_}.rs"
 done
 
+[ RUNTIME != "bajun" ] && exit 0
 CUSTOM_PALLETS=(
   "pallet-ajuna-awesome-avatars"
 )
