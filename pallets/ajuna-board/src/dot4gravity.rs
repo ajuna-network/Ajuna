584e24c81b27b351666948281d79eda74484502c
Eric <cowboy-bebug@users.noreply.github.com>
2022-12-05 17:00:03 +1300

    feat(board): restore from release/bengal

---
+++ pallets/ajuna-board/src/dot4gravity.rs
@@ -0,0 +1,86 @@
+// Ajuna Node
+// Copyright (C) 2022 BlogaTech AG
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use ajuna_common::{Finished, TurnBasedGame};
+use dot4gravity::Game as Dot4Gravity;
+pub use dot4gravity::{Coordinates, GameState, Side};
+use frame_support::pallet_prelude::*;
+use sp_std::borrow::ToOwned;
+
+#[derive(Clone, Eq, PartialEq, Encode, Decode, RuntimeDebugNoBound, TypeInfo, MaxEncodedLen)]
+pub enum Turn {
+	DropBomb(Coordinates),
+	DropStone((Side, u8)),
+}
+
+pub struct Game<Account>(PhantomData<Account>);
+impl<Account> TurnBasedGame for Game<Account>
+where
+	Account: Parameter,
+{
+	type Turn = Turn;
+	type Player = Account;
+	type State = GameState<Account>;
+
+	fn init(players: &[Self::Player], seed: Option<u32>) -> Option<Self::State> {
+		if let [player_1, player_2] = players {
+			Some(Dot4Gravity::new_game(player_1.to_owned(), player_2.to_owned(), seed))
+		} else {
+			None
+		}
+	}
+
+	fn get_last_player(state: &Self::State) -> Self::Player {
+		state
+			.last_move
+			.clone()
+			.map(|last_move_of| last_move_of.player)
+			.unwrap_or_else(|| state.next_player.clone())
+	}
+
+	fn get_next_player(state: &Self::State) -> Self::Player {
+		state.next_player.clone()
+	}
+
+	fn play_turn(
+		player: Self::Player,
+		state: Self::State,
+		turn: Self::Turn,
+	) -> Option<Self::State> {
+		match turn {
+			Turn::DropBomb(coords) => Dot4Gravity::drop_bomb(state, coords, player),
+			Turn::DropStone((side, pos)) => Dot4Gravity::drop_stone(state, player, side, pos),
+		}
+		.ok()
+	}
+
+	fn abort(state: Self::State, winner: Self::Player) -> Self::State {
+		let mut state = state;
+		state.winner = Some(winner);
+		state
+	}
+
+	fn is_finished(state: &Self::State) -> Finished<Self::Player> {
+		match state.winner.clone() {
+			Some(winner) => Finished::Winner(winner),
+			None => Finished::No,
+		}
+	}
+
+	fn seed(state: &Self::State) -> Option<u32> {
+		Some(state.seed)
+	}
+}
---
+++ pallets/ajuna-board/src/guessing.rs
@@ -0,0 +1,99 @@
+// Rules
+// One player can only have one go at a time
+// It's a guessing game where a player has to guess the right number
+// Initial state will have this number
+
+use crate::{self as pallet_ajuna_board};
+use ajuna_common::TurnBasedGame;
+use codec::{Decode, Encode};
+use frame_support::{pallet_prelude::MaxEncodedLen, RuntimeDebugNoBound};
+use scale_info::TypeInfo;
+use sp_std::marker::PhantomData;
+
+pub const THE_NUMBER: Guess = 42;
+pub type Guess = u32;
+pub struct MockGame<Account>(PhantomData<Account>);
+
+const MAX_PLAYERS: usize = 2;
+
+#[derive(Clone, Eq, PartialEq, Encode, Decode, RuntimeDebugNoBound, TypeInfo, MaxEncodedLen)]
+pub struct GameState<Account>
+where
+	Account: Clone + Encode + Decode + sp_std::fmt::Debug,
+{
+	pub players: [Account; MAX_PLAYERS],
+	pub next_player: u8,
+	pub solution: Guess,
+	pub winner: Option<Account>,
+}
+
+impl<Account> TurnBasedGame for MockGame<Account>
+where
+	Account: Clone + Encode + Decode + sp_std::fmt::Debug + PartialEq,
+{
+	type Turn = Guess;
+	type Player = Account;
+	type State = GameState<Self::Player>;
+
+	fn init(players: &[Self::Player], _seed: Option<u32>) -> Option<Self::State> {
+		match players.to_vec().try_into() {
+			Ok(players) =>
+				Some(GameState { players, next_player: 0, solution: THE_NUMBER, winner: None }),
+			_ => None,
+		}
+	}
+
+	fn get_last_player(state: &Self::State) -> Self::Player {
+		let next_player_index = (state.next_player as usize + 1) % state.players.len();
+		state.players[next_player_index].clone()
+	}
+
+	fn get_next_player(state: &Self::State) -> Self::Player {
+		state.players[state.next_player as usize].clone()
+	}
+
+	fn play_turn(
+		player: Self::Player,
+		state: Self::State,
+		turn: Self::Turn,
+	) -> Option<Self::State> {
+		if state.winner.is_some() {
+			return None
+		}
+
+		if !state.players.contains(&player) {
+			return None
+		}
+
+		if state.players[state.next_player as usize] != player {
+			return None
+		}
+
+		let mut state = state;
+		state.next_player = (state.next_player + 1) % state.players.len() as u8;
+
+		if state.solution == turn {
+			state.winner = Some(player);
+		}
+
+		Some(state)
+	}
+
+	fn abort(state: Self::State, winner: Self::Player) -> Self::State {
+		let mut state = state;
+		state.winner = Some(winner);
+		state
+	}
+
+	fn is_finished(state: &Self::State) -> pallet_ajuna_board::Finished<Self::Player> {
+		let winner = &state.winner;
+		match winner.clone() {
+			None => pallet_ajuna_board::Finished::No,
+			Some(winner) => pallet_ajuna_board::Finished::Winner(winner),
+		}
+	}
+
+	fn seed(_state: &Self::State) -> Option<u32> {
+		None
+	}
+}
