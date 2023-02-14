dfe7d414eae4b70db2ca7d92a8969d3ac405bbea
Dídac Sementé Fernández <didac@ajuna.io>
2023-02-14 10:25:02 +0100

    * Pallet nft staking implementation

--- runtime/bajun/src/lib.rs
+++ runtime/bajun/src/lib.rs
@@ -287,7 +287,8 @@ impl Contains<RuntimeCall> for BaseCallFilter {
 			RuntimeCall::Council(_) |
 			RuntimeCall::CouncilMembership(_) |
 			// ajuna pallets
-			RuntimeCall::AwesomeAvatars(_	) => true,
+			RuntimeCall::AwesomeAvatars(_	) |
+			RuntimeCall::NFTStake(_) => true,
 			RuntimeCall::Nft(_) => false,
 		}
 	}
@@ -764,6 +765,36 @@ impl pallet_ajuna_nft_transfer::Config for Runtime {
 	type NftHelper = Nft;
 }
 
+parameter_types! {
+	pub const NFTStakeTreasuryPalletId: PalletId = PalletId(*b"aj/nftst");
+	pub const MinimumStakingTokenReward: Balance = 100;
+	pub ContractCollectionConfig: CollectionConfig = CollectionConfig::default();
+	pub ContractCollectionItemConfig: pallet_nfts::ItemConfig = pallet_nfts::ItemConfig::default();
+}
+
+pub type CollectionConfig = pallet_nfts::CollectionConfig<Balance, BlockNumber, CollectionId>;
+
+pub type ContractAttributeKey = u32;
+pub type ContractAttributeValue = u64;
+
+impl pallet_ajuna_nft_staking::Config for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+	type Currency = Balances;
+	type CollectionId = CollectionId;
+	type CollectionConfig = CollectionConfig;
+	type ItemId = ItemId;
+	type ItemConfig = pallet_nfts::ItemConfig;
+	type NFTHelper = NFT;
+	type StakingOrigin = EnsureSigned<AccountId>;
+	type TreasuryPalletId = NFTStakeTreasuryPalletId;
+	type MinimumStakingTokenReward = MinimumStakingTokenReward;
+	type ContractCollectionConfig = ContractCollectionConfig;
+	type ContractCollectionItemConfig = ContractCollectionItemConfig;
+	type ContractAttributeKey = ContractAttributeKey;
+	type ContractAttributeValue = ContractAttributeValue;
+	type WeightInfo = pallet_ajuna_nft_staking::weights::AjunaWeight<Runtime>;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -816,6 +847,7 @@ construct_runtime!(
 		// Indexes 100+ should be reserved for NFT related pallets
 		Nft: pallet_nfts::{Pallet, Call, Storage, Event<T>} = 100,
 		NftTransfer: pallet_ajuna_nft_transfer = 101,
+		NFTStake: pallet_ajuna_nft_staking = 102,
 	}
 );
 
@@ -843,6 +875,7 @@ mod benches {
 		[pallet_scheduler, Scheduler]
 		[pallet_ajuna_awesome_avatars, AwesomeAvatars]
 		[pallet_nfts, Nft]
+		[pallet_ajuna_nft_staking, NFTStake]
 	);
 }
 
