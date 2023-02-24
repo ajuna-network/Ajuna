7d0387658c2c3c3428689864124d5c1f95ae6560
Eric <cowboy-bebug@users.noreply.github.com>
2023-02-24 15:36:32 +1300

    feat(aaa): add v2 migration

--- runtime/bajun/src/lib.rs
+++ runtime/bajun/src/lib.rs
@@ -121,7 +121,7 @@ pub type Executive = frame_executive::Executive<
 	Migrations,
 >;
 
-type Migrations = (pallet_ajuna_awesome_avatars::migration::v1::MigrateToV1<Runtime>,);
+type Migrations = (pallet_ajuna_awesome_avatars::migration::v2::MigrateToV2<Runtime>,);
 
 /// Handles converting a weight scalar to a fee value, based on the scale and granularity of the
 /// node's balance type.
