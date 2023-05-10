43e045ef560a168af40e16f0d39b6260895c661c
Eric <cowboy-bebug@users.noreply.github.com>
2023-05-11 07:35:24 +1200

    feat(bajun-runtime): bump spec_version to 118

--- runtime/bajun/src/lib.rs
+++ runtime/bajun/src/lib.rs
@@ -174,7 +174,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bajun"),
 	impl_name: create_runtime_str!("bajun"),
 	authoring_version: 1,
-	spec_version: 117,
+	spec_version: 118,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
