4e7bf0f2488c827a5a67c1e4706a6b37a550df9f
Eric <cowboy-bebug@users.noreply.github.com>
2023-05-30 13:28:31 +1200

    feat(bajun-runtime): configure v5 migration

--- runtime/bajun/src/lib.rs
+++ runtime/bajun/src/lib.rs
@@ -120,7 +120,7 @@ pub type Executive = frame_executive::Executive<
 	Migrations,
 >;
 
-type Migrations = (pallet_ajuna_awesome_avatars::migration::v4::MigrateToV4<Runtime>,);
+type Migrations = (pallet_ajuna_awesome_avatars::migration::v5::MigrateToV5<Runtime>,);
 
 /// Handles converting a weight scalar to a fee value, based on the scale and granularity of the
 /// node's balance type.
