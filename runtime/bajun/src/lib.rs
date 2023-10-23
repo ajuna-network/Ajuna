52e93f38181fa225a59ede887d2f1ac564e8fe0e
DidacSF <didac@ajuna.io>
2023-10-23 09:23:17 +0200

    Release/0.1.25 (#360)
    * Release 0.1.25

--- runtime/bajun/src/lib.rs
+++ runtime/bajun/src/lib.rs
@@ -161,7 +161,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bajun"),
 	impl_name: create_runtime_str!("bajun"),
 	authoring_version: 1,
-	spec_version: 124,
+	spec_version: 125,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
