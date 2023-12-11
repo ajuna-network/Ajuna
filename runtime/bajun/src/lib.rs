9744028ac20c5e49278667d14723a600d2690231
Dídac Sementé Fernández <didac@ajuna.io>
2023-12-11 15:47:57 +0100

    * Restored UNINCLUDED_SEGMENT_CAPACITY

--- runtime/bajun/src/lib.rs
+++ runtime/bajun/src/lib.rs
@@ -211,7 +211,7 @@ const MAXIMUM_BLOCK_WEIGHT: Weight = Weight::from_parts(
 
 /// Maximum number of blocks simultaneously accepted by the Runtime, not yet included
 /// into the relay chain.
-const UNINCLUDED_SEGMENT_CAPACITY: u32 = 1;
+const UNINCLUDED_SEGMENT_CAPACITY: u32 = 3;
 /// How many parachain blocks are processed by the relay chain per parent. Limits the
 /// number of blocks authored per slot.
 const BLOCK_PROCESSING_VELOCITY: u32 = 1;
