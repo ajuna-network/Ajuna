f9501f8b3b19b767d60a44017cc3943f16e1dda4
Dídac Sementé Fernández <didac@ajuna.io>
2023-12-11 12:12:21 +0100

    * Release 0.1.27

--- runtime/ajuna/src/lib.rs
+++ runtime/ajuna/src/lib.rs
@@ -153,7 +153,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("ajuna"),
 	impl_name: create_runtime_str!("ajuna"),
 	authoring_version: 1,
-	spec_version: 126,
+	spec_version: 127,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
